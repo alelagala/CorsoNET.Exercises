6ad81109769dff41a5f4f31e62ca09cadf22933d
Alessandro <alessandro.lagala97@gmail.com>
2022-10-25 11:02:07 +0200

    Aggiungere i file di progetto.

---
+++ Eurozone/Abstracts/Territory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eurozone
+{
+    public abstract class Territory
+    {
+        protected int popolazione;
+        protected float areaGeografica;
+        protected string continente;
+
+        public Territory(int _popolazione, float _areaGeografica, string _continente)
+        {
+            popolazione = _popolazione;
+            areaGeografica = _areaGeografica;
+            continente = _continente;
+        }
+    }
+}
---
+++ Eurozone/Country/Country.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eurozone
+{
+    public class Country : Territory
+    {
+        protected string costituzione;
+        protected string bandiera;
+        protected string moneta;
+        protected string linguaUfficiale;
+        protected float pil;
+        protected bool penaMorte;
+        protected string nome;
+
+        public Country(int popolazione, float areaGeografica, string continente, string _nome, string _costituzione, string _bandiera, string _moneta, string _linguaUfficiale, float _pil, bool _penaMorte) : base(popolazione, areaGeografica, continente)
+        {
+            nome = _nome;
+            costituzione = _costituzione;
+            bandiera = _bandiera;
+            moneta = _moneta;
+            linguaUfficiale = _linguaUfficiale;
+            pil = _pil;
+            penaMorte = _penaMorte;
+        }
+    }
+}
+
+
