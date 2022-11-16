fd0b6346ae3d286e1c5b5ed0e46415e4f3ffafce
Alessandro <alessandro.lagala97@gmail.com>
2022-11-16 12:53:50 +0100

    "added esercizio file system"

---
+++ Esercizio FileSystem/Program.cs
@@ -0,0 +1,102 @@
+﻿using Esercizio_FileSystem;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Esercizio_FileSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>() { new Person("Ale","La Gala",25),new Person("Marco","Sabato",89) };
+            List<Account> accounts = new List<Account>() { new Account(928, "BancoPosta"), new Account(2873, "Postepay") };
+            WriteOnFile(persons);
+            WriteOnFile(accounts);
+
+            
+
+
+
+
+
+        }
+        public static void WriteOnFile<T>(List<T> list)
+        {
+            
+            if (list is List<Person>)
+            {
+                for(int i = 0; i < list.Count; i++)
+                {
+                    var person = list[i] as Person;
+                    WritePerson(person);
+                }                
+            }
+            else if (list is List<Account>)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    var account = list[i] as Account;
+                    WriteAccount(account);
+                }
+            }
+        }
+
+        public static void WritePerson(Person person)
+        {
+            string current = Environment.CurrentDirectory;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Name:{person.Name}, Surname:{person.Surname},Age:{person.Age}");
+            File.AppendAllText(Path.Combine(current, "persons.txt"), sb.ToString());
+        }
+        public static void WriteAccount(Account account)
+        {
+            string current = Environment.CurrentDirectory;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Account id:{account.Id}, Account name:{account.Name}");
+            File.AppendAllText(Path.Combine(current, "accounts.txt"), sb.ToString());
+        }
+
+
+
+    }
+
+    
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }   
+    }
+
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Account(int id,string name)
+        {
+            Id = id;
+            Name= name;
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
