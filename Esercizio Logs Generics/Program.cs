f9044886fc6062600da1addc26544c1368ceb843
Alessandro <alessandro.lagala97@gmail.com>
2022-11-17 15:55:03 +0100

    "aggiunta funzione loadfromfile e checktype"

--- Esercizio Logs Generics/Program.cs
+++ Esercizio Logs Generics/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Esercizio_Logs_Generics
 {
@@ -8,20 +11,19 @@ namespace Esercizio_Logs_Generics
     {
         static void Main(string[] args)
         {
-            Person alex = new("Alex","Rossi");
-            Account account1 = new Account("Bancomat", 374908276);
-            Log(alex);
-            Log(account1);
+            Person alex = new() { Name="Alex",Surname="Rossi"};
+            Account account1 = new() { Name="Bancomat", Id=374908276 };
+
+            string path = Path.Combine(Environment.CurrentDirectory, $"{alex.GetType().Name}.csv");
+            List<Person> list = new List<Person>();
+            list=LoadFromFile<Person>(path);
         }
-
-
-
 
         public static void Log<T>(T item) where T : class
         {
             var properties= item.GetType().GetProperties();
             var itemName = item.GetType().Name;
-            string path = Path.Combine(Environment.CurrentDirectory,$"{itemName}.txt");
+            string path = Path.Combine(Environment.CurrentDirectory,$"{itemName}.csv");
             if (!File.Exists(path))
             {
                 foreach(var property in properties)
@@ -38,8 +40,50 @@ namespace Esercizio_Logs_Generics
                 File.AppendAllText(path, property.GetValue(item).ToString());
                 File.AppendAllText(path, " ");
             }
-            File.AppendAllText(path, "\n");
+            //File.AppendAllText(path, "\n");
         }
+    
+        public static List<T> LoadFromFile<T>(string path) where T: class, new()
+        {
+            List<T>output = new List<T>();
+            var lines = File.ReadAllLines(path).ToList();            
+            T entry=new();
+            string[] headers= lines[0].Split(';');
+            lines.RemoveAt(0);
+            int i;
+            if (checkType(entry, path))
+            { 
+                foreach (var line in lines)
+                {
+                    i = 0;
+                    string[] values = line.Split(';');
+                    entry = new T();
+                    foreach (var value in values)
+                    {
+                        //var converted = Convert.ChangeType(value, entry.GetType().GetProperty(headers[i]).PropertyType);
+                        entry.GetType().GetProperty(headers[i]).SetValue(entry, Convert.ChangeType(value, entry.GetType().GetProperty(headers[i]).PropertyType));
+                        i++;
+                    }
+                    output.Add(entry);
+                }
+            }
+            else { Console.WriteLine("File non compatibile"); }
+            return output;
+        }
+
+
+        public static bool checkType<T>(T item, string path) where T:class,new()
+        {
+            string[] headers = File.ReadAllLines(path).ToList()[0].Split(';');
+            foreach(string header in headers)
+            {
+                if (item.GetType().GetProperty(header) == null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }    
     }
 
 
@@ -48,10 +92,8 @@ namespace Esercizio_Logs_Generics
         public string Name { get; set; }
         public string Surname { get; set; }
         
-        public Person(string name,string surname)
+        public Person()
         {
-            Name = name;
-            Surname= surname;
         }
     }
     
@@ -59,10 +101,8 @@ namespace Esercizio_Logs_Generics
     {
         public string Name { get; set; }
         public int Id { get; set; }
-        public Account(string name, int id)
+        public Account()
         {
-            Name = name;
-            Id = id;
         }
     }
 
